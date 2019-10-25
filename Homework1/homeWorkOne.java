52f45bc5954f009e91949f4860b98abf9109193e
dxni <33109901+dxni@users.noreply.github.com>
2019-10-25 17:32:03 -0400

    Fixes
    - I fixed the return of checkPalindrome fuction
    - I fixed code format

--- Homework1/homeWorkOne.java
+++ Homework1/homeWorkOne.java
@@ -1,55 +1,36 @@
-public class homeWorkOne{
-    public static void main (String []args){
-        int n= 2;
-        String word= "a";
-         
-        System.out.println("El resultado es: " +largestNumber(n));
-        int year = 2012;
-        System.out.println("el centenario es: "+centuryFromYear(year));
-
-        if (checkPalindrome(word)== true){
-            System.out.println("La Palabra "+word+" ...es Palindrome");
+public class homeWorkOne {
+    public static void main(String[] args) {
+        int n = 2;
+        String word = "adsd";
+        System.out.println("El resultado es: " + largestNumber(n));
+        int year = 1790;
+        System.out.println("el centenario es: " + centuryFromYear(year));
+        if (checkPalindrome(word) == true) {
+            System.out.println("La Palabra " + word + " ...es Palindrome");
+        } else {
+            System.out.println("La Palabra " + word + " ...No es Palindrome");
         }
-        else{
-            System.out.println("La Palabra "+word+" ...No es Palindrome");
-        }
+    }
 
+    public static int largestNumber(int n) {
 
+        return (int) Math.pow(10, n) - 1;
+    }
 
-
-}
-
-public static int largestNumber(int n){
-        
-    return (int) Math.pow(10, n)-1;
-}
-
-public static int centuryFromYear(int year){
-    return ((year-1)/100)+1;
-
-}
-
-public static String invertirPalabra(String word){
-    String aux="";
-    for (int i=word.length()-1; i>=0 ; i--){
-            aux=aux+ word.charAt(i);
+    public static int centuryFromYear(int year) {
+        return ((year - 1) / 100) + 1;
 
     }
-    
-    return aux;
 
-}
-
-public static boolean checkPalindrome (String word){
-    
-    
-    if (word.equals(invertirPalabra(word))){
-        return true;
+    public static String invertirPalabra(String word) {
+        String aux = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            aux = aux + word.charAt(i);
+        }
+        return aux;
     }
-    else
-    {
-        return false;
-}
-}    
 
+    public static boolean checkPalindrome(String word) {
+        return word.equals(invertirPalabra(word));
+    }
 }
